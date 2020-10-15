cfbf940053a471b30c580b44ac257b8f6ce14680
Alper Çalışkan <alpercaliskan96@gmail.com>
2020-10-15 05:28:18 +0300

    Add new controller and view + _AdminLayout

---
+++ Controllers/KategoriController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_TicariOtomasyon.Models.Siniflar;
+
+namespace MVC_TicariOtomasyon.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var value = c.Kategoris.ToList();
+            return View(value);
+        }
+    }
+}
