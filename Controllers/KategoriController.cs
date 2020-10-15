7c43687a2dfe7c85f23e15b3fc66aa5281b01a29
Alper Çalışkan <alpercaliskan96@gmail.com>
2020-10-15 06:34:55 +0300

    Add KategoriController & Views

--- Controllers/KategoriController.cs
+++ Controllers/KategoriController.cs
@@ -16,5 +16,41 @@ namespace MVC_TicariOtomasyon.Controllers
             var value = c.Kategoris.ToList();
             return View(value);
         }
+        //GET 
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+        //POST KISMI
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategori k)
+        {
+            c.Kategoris.Add(k);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //KATEGORI SILME ISLEMI
+        public ActionResult KategoriSil(int id)
+        {
+            var ktg = c.Kategoris.Find(id);
+            c.Kategoris.Remove(ktg);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //KATEGORI GETIRME ISLEMI
+        public ActionResult KategoriDuzenle(int id)
+        {
+            var ktg = c.Kategoris.Find(id);
+            return View("KategoriDuzenle", ktg);
+        }
+        //KATEGORI GUNCELLEME ISLEMI
+        public ActionResult KategoriGuncelle(Kategori k)
+        {
+            var ktg = c.Kategoris.Find(k.KategoriID);
+            ktg.KategoriAd = k.KategoriAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
