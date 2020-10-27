704a95bae6a92a01675ab56c662adc931cf48939
Alper Çalışkan <alpercaliskan96@gmail.com>
2020-10-27 03:47:03 +0300

    Add galleryController & Views

---
+++ Controllers/GaleriController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_TicariOtomasyon.Models.Siniflar;
+
+namespace MVC_TicariOtomasyon.Controllers
+{
+    public class GaleriController : Controller
+    {
+        Context c = new Context();
+        // GET: Galeri
+        public ActionResult Index()
+        {
+            var value = c.Urunlers.ToList();
+            return View(value);
+        }
+    }
+}
---
+++ Controllers/UrunDetayController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_TicariOtomasyon.Models.Siniflar;
+
+namespace MVC_TicariOtomasyon.Controllers
+{
+    public class UrunDetayController : Controller
+    {
+        Context c = new Context();
+        // GET: UrunDetay
+        public ActionResult Index()
+        {
+            var value = c.Urunlers.Where(x => x.UrunID == 1).ToList();
+            return View(value);
+        }
+    }
+}
